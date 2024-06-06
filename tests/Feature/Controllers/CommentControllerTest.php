48b0d5b5447ab7df245711c1b2330f0788b19ae8
Milad Rezaeian <milad.rezaeiann@gmail.com>
2024-06-07 00:49:59 +0330

    implement comment controller test - store method when user is logged in

--- tests/Feature/Controllers/CommentControllerTest.php
+++ tests/Feature/Controllers/CommentControllerTest.php
@@ -25,4 +25,18 @@ class CommentControllerTest extends TestCase
         $response->assertSessionHasErrors('body');
     }
 
+    public function test_store_method_when_user_logged_in()
+    {
+        $user = User::factory()->create();
+        $image = Image::factory()->create();
+        $data = Comment::factory()->state([
+            'user_id' => $user->id,
+            'image_id' => $image->id,
+        ])->make()->toArray();
+        $comment = Comment::factory()->make($data);
+        $comment->save();
+
+        $this->assertDatabaseHas('comments', $data);
+    }
+
 }
